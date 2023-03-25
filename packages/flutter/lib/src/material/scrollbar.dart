9fa5ca327d5eeb169387314183aa349f358e0c07
Greg Price <greg@zulip.com>
2023-03-25 14:12:25 -0700

    WIP debugging in material/scrollbar

--- packages/flutter/lib/src/material/scrollbar.dart
+++ packages/flutter/lib/src/material/scrollbar.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 
 import 'color_scheme.dart';
@@ -214,6 +215,8 @@ class Scrollbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // print(debugDefaultTargetPlatformOverride);
+    // print(Theme.of(context).platform);
     if (Theme.of(context).platform == TargetPlatform.iOS) {
       return CupertinoScrollbar(
         thumbVisibility: isAlwaysShown ?? thumbVisibility ?? false,
