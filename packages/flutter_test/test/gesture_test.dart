2e4677a0dd87a7178cc9c9d976dd7c372ad47452
Greg Price <greg@zulip.com>
2023-04-29 16:14:24 -0700

    reduce more

--- packages/flutter_test/test/gesture_test.dart
+++ packages/flutter_test/test/gesture_test.dart
@@ -8,35 +8,25 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   Future<void> startDrag(WidgetTester tester, {required int pointer}) async {
-    await tester.pumpWidget(
-      MaterialApp(
-        home: Column(
-          children: <Widget>[
-            LongPressDraggable<int>(
-              feedback: GestureDetector(onTap: () {}, child: const SizedBox.shrink()),
-              child: const Text('Target'),
-            ),
-          ],
-        ),
+    await tester.pumpWidget(MaterialApp(home: Column(children: <Widget>[
+      LongPressDraggable<int>(
+        feedback: GestureDetector(onTap: () {}, child: const SizedBox.shrink()),
+        child: const Text('Target'),
       ),
-    );
+    ])));
 
     await tester.startGesture(pointer: pointer, tester.getCenter(find.text('Target')));
     await tester.pump(kLongPressTimeout);
   }
 
   Future<void> completeDrag(WidgetTester tester, {required int pointer}) async {
-    await tester.pumpWidget(MaterialApp(
-      home: Column(
-        children: <Widget>[
-          const Draggable<int>(data: 1, feedback: SizedBox.shrink(), child: Text('Source')),
-          DragTarget<int>(
-            builder: (BuildContext _, List<int?> __, List<dynamic> ___) => const Text('Target'),
-            onAccept: (int data) {},
-          ),
-        ],
+    await tester.pumpWidget(MaterialApp(home: Column(children: <Widget>[
+      const Draggable<int>(data: 1, feedback: SizedBox.shrink(), child: Text('Source')),
+      DragTarget<int>(
+        builder: (BuildContext _, List<int?> __, List<dynamic> ___) => const Text('Target'),
+        onAccept: (int data) {},
       ),
-    ));
+    ])));
 
     final TestGesture gesture = await tester.startGesture(
       pointer: pointer, tester.getCenter(find.text('Source')));
