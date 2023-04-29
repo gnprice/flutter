d37d37aa66ca9a843287ee94e64025841493378e
Greg Price <greg@zulip.com>
2023-04-29 16:25:15 -0700

    add group

--- packages/flutter_test/test/gesture_test.dart
+++ packages/flutter_test/test/gesture_test.dart
@@ -7,40 +7,47 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  Future<void> startDrag(WidgetTester tester, {required int pointer}) async {
-    await tester.pumpWidget(MaterialApp(home: Column(children: <Widget>[
-      LongPressDraggable<int>(
-        feedback: GestureDetector(onTap: () {}, child: const SizedBox.shrink()),
-        child: const Text('Target'),
-      ),
-    ])));
+  group('No gesture state leak in draggables', () {
+    Future<void> startDrag(WidgetTester tester, {required int pointer}) async {
+      await tester.pumpWidget(MaterialApp(home: Column(children: <Widget>[
+        LongPressDraggable<int>(
+          feedback: GestureDetector(onTap: () {}, child: const SizedBox.shrink()),
+          child: const Text('Target'),
+        ),
+      ])));
 
-    await tester.startGesture(pointer: pointer, tester.getCenter(find.text('Target')));
-    await tester.pump(kLongPressTimeout);
-  }
+      await tester.startGesture(pointer: pointer, tester.getCenter(find.text('Target')));
+      await tester.pump(kLongPressTimeout);
+      // Then don't end the gesture.  This would cause a state leak
+      // if the tester didn't automatically clean it up.
+    }
 
-  Future<void> completeDrag(WidgetTester tester, {required int pointer}) async {
-    await tester.pumpWidget(MaterialApp(home: Column(children: <Widget>[
-      const Draggable<int>(data: 1, feedback: SizedBox.shrink(), child: Text('Source')),
-      DragTarget<int>(
-        builder: (BuildContext _, List<int?> __, List<dynamic> ___) => const Text('Target'),
-        onAccept: (int data) {},
-      ),
-    ])));
+    Future<void> completeDrag(WidgetTester tester, {required int pointer}) async {
+      await tester.pumpWidget(MaterialApp(home: Column(children: <Widget>[
+        const Draggable<int>(data: 1, feedback: SizedBox.shrink(), child: Text('Source')),
+        DragTarget<int>(
+          builder: (BuildContext _, List<int?> __, List<dynamic> ___) => const Text('Target'),
+          onAccept: (int data) {},
+        ),
+      ])));
 
-    final TestGesture gesture = await tester.startGesture(
-      pointer: pointer, tester.getCenter(find.text('Source')));
-    await gesture.moveTo(tester.getCenter(find.text('Target')));
-    await gesture.up();
-  }
+      final TestGesture gesture = await tester.startGesture(
+          pointer: pointer, tester.getCenter(find.text('Source')));
+      await gesture.moveTo(tester.getCenter(find.text('Target')));
+      await gesture.up();
+    }
 
-  testWidgets('Start drag vs. complete drag, side A', (WidgetTester tester) async {
-    await startDrag(tester, pointer: 7);
-    await completeDrag(tester, pointer: 8);
-  });
+    // Logically these tests consist of startDrag in one test,
+    // then completeDrag in the other, on the same pointer.
 
-  testWidgets('Start drag vs. complete drag, side B', (WidgetTester tester) async {
-    await startDrag(tester, pointer: 8);
-    await completeDrag(tester, pointer: 7);
+    testWidgets('side A', (WidgetTester tester) async {
+      await startDrag(tester, pointer: 7);
+      await completeDrag(tester, pointer: 8);
+    });
+
+    testWidgets('side B', (WidgetTester tester) async {
+      await startDrag(tester, pointer: 8);
+      await completeDrag(tester, pointer: 7);
+    });
   });
 }
