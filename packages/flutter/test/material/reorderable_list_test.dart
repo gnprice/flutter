c1bbb7457b80eec72b5b0f3d7db68659ad13a74a
Greg Price <greg@zulip.com>
2023-04-28 21:30:13 -0700

    wip clean up a gesture in material/reorderable_list_test

--- packages/flutter/test/material/reorderable_list_test.dart
+++ packages/flutter/test/material/reorderable_list_test.dart
@@ -1845,6 +1845,7 @@ void main() {
         duration: const Duration(milliseconds: 600),
         interval: Duration(milliseconds: (1000 / autoScrollerVelocityScalar).round()),
       );
+      await drag.up();
 
       return scrollController.offset;
     }
