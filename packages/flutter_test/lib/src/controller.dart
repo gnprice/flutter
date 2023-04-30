dd57190386424528d76490a94c07bcd77c76fbef
Greg Price <greg@zulip.com>
2023-04-29 22:11:38 -0700

    Revert "WIP debug-print in _getNextPointer"
    This reverts commit 71dd4d8ed5466c9d6f491521f9cf8621e8b00c62.

--- packages/flutter_test/lib/src/controller.dart
+++ packages/flutter_test/lib/src/controller.dart
@@ -1130,7 +1130,6 @@ abstract class WidgetController {
   int _getNextPointer() {
     final int result = _nextPointer;
     _nextPointer += 1;
-    print("nextPointer: $result");
     return result;
   }
 
