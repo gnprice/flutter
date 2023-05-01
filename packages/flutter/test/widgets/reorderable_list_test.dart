ede1e55877f2d1e86016d9fd0428bda7afd90386
Greg Price <greg@zulip.com>
2023-04-30 20:56:50 -0700

    test no leaks

--- packages/flutter/test/widgets/reorderable_list_test.dart
+++ packages/flutter/test/widgets/reorderable_list_test.dart
@@ -252,6 +252,44 @@ void main() {
     expect(tester.takeException(), isNull);
   });
 
+  testWidgets("Don't leak gesture recognizer on duplicate listener", (WidgetTester tester) async {
+    final List<int> items = <int>[1, 2, 3];
+
+    await tester.pumpWidget(MaterialApp(
+      home: ReorderableList(
+        itemBuilder: (BuildContext context, int index) {
+          return ReorderableDelayedDragStartListener(
+            index: index,
+            key: ValueKey<int>(items[index]),
+            child: SizedBox(
+              height: 100,
+              child: ReorderableDragStartListener(
+                index: index,
+                child: Text('item ${items[index]}'),
+              ),
+            ),
+          );
+        },
+        itemCount: items.length,
+        onReorder: (int from, int to) {},
+      ),
+    ));
+
+    int routeCount(int pointer) => GestureBinding.instance.pointerRouter.debugRouteCount(pointer);
+
+    expect(routeCount(1), 0);
+    final TestGesture drag = await tester.startGesture(pointer: 1, tester.getCenter(find.text('item 1')));
+    expect(routeCount(1), greaterThan(0));
+    await tester.pump(kPressTimeout);
+
+    await drag.moveBy(const Offset(0, 100));
+    await tester.pumpAndSettle();
+
+    expect(routeCount(1), greaterThan(0));
+    await tester.pumpWidget(const SizedBox.shrink());
+    expect(routeCount(1), 0);
+  });
+
   testWidgets('negative itemCount should assert', (WidgetTester tester) async {
     final List<int> items = <int>[1, 2, 3];
     await tester.pumpWidget(MaterialApp(
